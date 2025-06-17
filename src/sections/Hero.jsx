46f39995f816a02eb6d97f3f9943fda232306b9a
ajmalcm <ajmalcm22@gmail.com>
2025-06-17 16:30:27 +0530

    progressing with hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Herotext from '../Components/Herotext'
+import ParallaxBg from '../Components/ParallaxBg'
 
 const Hero = () => {
   return (
     <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
     <Herotext/>
+    <ParallaxBg/>
+    <figure className='absolute inset-0' style={{width:"100vw",height:"100vh"}}></figure>
     </section>
   )
 }
