bfa31f46e89e344dce78e593dc40a54a5cd533c2
ajmalcm <ajmalcm22@gmail.com>
2025-06-19 10:20:38 +0530

    flaoting astronomor done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
 import Herotext from '../Components/Herotext'
 import ParallaxBg from '../Components/ParallaxBg'
+import { Canvas } from '@react-three/fiber'
+import {Astronaut} from '../Components/Astronaut'
+import { OrbitControls } from '@react-three/drei'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+
+  const isMobile=useMediaQuery({maxWidth:853})
+
   return (
     <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
     <Herotext/>
     <ParallaxBg/>
-    <figure className='absolute inset-0' style={{width:"100vw",height:"100vh"}}></figure>
+    <figure className='absolute inset-0' style={{width:"100vw",height:"100vh"}}>
+      <Canvas camera={{position:[0,1,3]}}>
+        <Astronaut scale={ isMobile && 0.23} position={isMobile && [0,-1.5,0]}/>
+        <OrbitControls/>
+      </Canvas>
+    </figure>
     </section>
   )
 }
