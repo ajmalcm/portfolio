1cefe790e69ef487ef764f38490f38ef36286d56
ajmal <ajmalcm22@gmail.com>
2025-06-21 02:19:55 +0530

    about section in Progress

---
+++ src/sections/About.jsx
@@ -0,0 +1,95 @@
+import { useRef } from "react";
+import Card from "../Components/Card";
+import { Globe } from "../Components/Globe";
+
+const About = () => {
+  const grid2Container = useRef();
+  return (
+    <section className="c-space section-spacing">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        {/* grid1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Ajmal C.M</p>
+            <p className="subtext">
+              Over the past two years, I’ve been crafting responsive,
+              user-friendly web apps using React, Next.js, and Tailwind CSS. I’m
+              passionate about building sleek digital experiences that are both
+              functional and intuitive.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 pointer-events-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo"></div>
+        </div>
+        {/* grid2 */}
+        <div className="grid-default-color grid-2" ref={grid2Container}>
+          <div className="flex items-center justify-center w-full h-full">
+            <p className="flex items-end text-5xl text-gray-500">
+              CODE IS CRAFT
+            </p>
+            <Card
+              style={{ rotate: "75deg", top: "30%", left: "20%" }}
+              text="GRASP"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              text="SOLID"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+              text="Design Pattern"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              text="Design Principles"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
+              text="SRP"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/csharp-pink.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/dotnet-pink.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-75deg", top: "10%", right: "20%" }}
+              image="assets/logos/blazor-pink.png"
+              containerRef={grid2Container}
+            />
+          </div>
+        </div>
+        {/* grid3 */}
+        <div className="grid-black-color grid-3">
+            <div className="z-10 w-[50%]">
+                <p className="head-text">Time Zone</p>
+                <p className="subtext">I'm based in India, and open to remote work worldwide</p>
+            </div>
+            <figure className="absolute left-[30%] top-[10%]">
+                <Globe/>
+            </figure>
+        </div>
+        {/* grid4 */}
+        <div className="grid-special-color grid-4"></div>
+        {/* grid5 */}
+        <div className="grid-default-color grid-5"></div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
